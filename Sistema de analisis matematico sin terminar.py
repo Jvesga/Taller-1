516e960df4fc478900208b31c9e22e02c4b8ddfa
Jorge Alejandro Vesga Quintero <102545608+Jvesga@users.noreply.github.com>
2025-08-12 20:42:40 -0500

    Add files via upload

---
+++ Sistema de analisis matematico sin terminar.py
@@ -0,0 +1,4 @@
+#Sistema de analisis matematico#
+#Un sistema de análisis matemático requiere clasificar valores según su posición respecto a cero en la recta numérica, determinando si es positivo, negativo o neutro.# 
+
+numero
