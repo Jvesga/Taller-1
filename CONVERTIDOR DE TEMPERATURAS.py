8d1467a5eda01f50b3a07eb6513a5f9fc11b13de
Jorge Alejandro Vesga Quintero <102545608+Jvesga@users.noreply.github.com>
2025-08-12 20:35:43 -0500

    Add files via upload

---
+++ CONVERTIDOR DE TEMPERATURAS.py
@@ -0,0 +1,12 @@
+### CONVERTIDOR DE TEMPERATURAS ###
+### Un laboratorio necesita convertir mediciones térmicas desde la escala Celsius hacia las escalas Fahrenheit (usada en países anglosajones) y Kelvin (escala absoluta científica). ###
+
+
+celcius= float(input("Digite la temperatura en grados celcius: "))
+
+fahrenheit= (celcius * 9/5) + 32 
+kelvin= celcius + 273.15
+
+print("La temperatura digitada es de" , celcius , "°")
+print("la conversion de temperatura a escala Fahrenheit es: " , fahrenheit , "°F" )
+print("la conversion de temperatura a escala Kelvin es: " , kelvin , "°K" )
---
+++ Tiempo_de_vida.py
@@ -0,0 +1,12 @@
+nombre1= (input("Digite el nombre del Primer usuario: "))
+edad1= int(input("Digite la edad del Primer Usuario: "))
+
+nombre2= (input("Digite la nombre del Segundo usuario: "))
+edad2= int(input("Digite la edad del Segundo usuario: "))
+
+if (edad1 > edad2):
+    print(nombre1 ,  "es mayor que " , nombre2)
+
+else:
+    edad2 > edad1
+    print(nombre2 , " es mayor que " , nombre1)
