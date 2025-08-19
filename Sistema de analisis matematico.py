1c868d2a26b4f997874936d08aa3847672598ea9
Jorge Alejandro Vesga Quintero <102545608+Jvesga@users.noreply.github.com>
2025-08-19 18:45:14 -0500

    Add files via upload

---
+++ Sistema de analisis matematico.py
@@ -0,0 +1,18 @@
+#Sistema de analisis matematico#
+#Un sistema de análisis matemático requiere clasificar valores según su posición respecto a cero en la recta numérica, determinando si es positivo, negativo o neutro.# 
+
+numero= int(input("Dijite el numero a comprobar: "))
+
+if(numero > 0):
+    print("El numero dijitado es positivo y esta en la posicion " , numero , "de la recta numerica. ")
+
+elif(numero < 0):
+    print("El numero digitado es negativo y esta en la posicion " , numero , "de la recta numerica. ")
+    
+else:
+    (numero == 0)
+    print("El numero digitado es neutro y esta en la posicion " , numero , "de la recta numerica. ")
+    
+
+
+    
