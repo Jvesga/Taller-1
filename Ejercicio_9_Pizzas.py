883841431c356128822e565d9738545ff9af555b
Jorge Alejandro Vesga Quintero <102545608+Jvesga@users.noreply.github.com>
2025-08-22 11:18:29 -0500

    Update Ejercicio_9_Pizzas.py

--- Ejercicio_9_Pizzas.py
+++ Ejercicio_9_Pizzas.py
@@ -1,9 +1,16 @@
-#9.	La pizzería Pequeña Italia ofrece pizzas vegetarianas y no vegetarianas a sus clientes. Los ingredientes para cada tipo de pizza aparecen a continuación.
+#La pizzería Pequeña Italia ofrece pizzas vegetarianas y 
+#no vegetarianas a sus clientes. 
+#Los ingredientes para cada tipo de pizza aparecen a continuación.
 
-    #Ingredientes vegetarianos: Pimiento y tofu.
-    #Ingredientes no vegetarianos: Peperoni, Jamón y Salmón.
+#Ingredientes vegetarianos: Pimiento y tofu.
+#Ingredientes no vegetarianos: Peperoni, Jamón y Salmón.
 
-#Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o no, y en función de su respuesta le muestre un menú con los ingredientes disponibles para que elija. Solo se puede elegir un ingrediente además de la mozzarella y el tomate que están en todas las pizzas. Al final se debe mostrar por pantalla si la pizza elegida es vegetariana o no y todos los ingredientes que lleva.
+#Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o no, 
+#y en función de su respuesta le muestre un menú con los 
+#ingredientes disponibles para que elija. Solo se puede 
+#elegir un ingrediente además de la mozzarella y el tomate que están en todas las pizzas. 
+#Al final se debe mostrar por pantalla 
+#si la pizza elegida es vegetariana o no y todos los ingredientes que lleva.
 
 print("Pequeña italia ofrece pizzas vegetarianas y no vegetarianas")
 pizza_cliente=input("Que tipo de pizza desea: ")
@@ -25,5 +32,3 @@ else:
     print("La pizza que desea no es vegetaria y nuestros ingredientes son" , "peperoni, jamon o salmon")
     b=input("Elija un ingrediente: ")
     print("La pizza que elijio no es Vegetariana y tiene los siguientes ingredientes:" , "mozzarella, tomate y " , b)
-
-
