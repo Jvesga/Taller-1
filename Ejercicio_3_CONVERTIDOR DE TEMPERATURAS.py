d0b855dfca41c2c7c92b1731197dfd216447fcd0
Jorge Alejandro Vesga Quintero <102545608+Jvesga@users.noreply.github.com>
2025-08-22 11:04:13 -0500

    Update Ejercicio_3_CONVERTIDOR DE TEMPERATURAS.py

--- Ejercicio_3_CONVERTIDOR DE TEMPERATURAS.py
+++ Ejercicio_3_CONVERTIDOR DE TEMPERATURAS.py
@@ -1,6 +1,7 @@
-### CONVERTIDOR DE TEMPERATURAS ###
-### Un laboratorio necesita convertir mediciones térmicas desde la escala Celsius hacia las escalas Fahrenheit (usada en países anglosajones) y Kelvin (escala absoluta científica). ###
-
+# CONVERTIDOR DE TEMPERATURAS #
+# Un laboratorio necesita convertir mediciones térmicas desde la escala 
+#Celsius hacia las escalas Fahrenheit (usada en países anglosajones) y 
+#Kelvin (escala absoluta científica). 
 
 celcius= float(input("Digite la temperatura en grados celcius: "))
 
@@ -10,4 +11,5 @@ kelvin= celcius + 273.15
 print("La temperatura digitada es de" , celcius , "°")
 print("la conversion de temperatura a escala Fahrenheit es: " , fahrenheit , "°F" )
 print("la conversion de temperatura a escala Kelvin es: " , kelvin , "°K" )
+
 
